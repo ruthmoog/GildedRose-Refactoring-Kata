851ed4d483dc431cbe8eaf8772fe4f95dca68951
Ruth Baker <ruthmoog@gmail.com>
2021-08-01 14:28:45 +0100

    Begin using combination approvals in tests

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,17 +1,25 @@
 package com.gildedrose;
 
+import org.approvaltests.Approvals;
+import org.approvaltests.combinations.CombinationApprovals;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GildedRoseTest {
 
     @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+    void updateQuality() {
+        CombinationApprovals.verifyAllCombinations(
+            this::doUpdateQuality,
+            new String[] {"Blue"},
+            new Integer[] {0},
+            new Integer[] {0});
     }
 
+    private String doUpdateQuality(String name, int sellIn, int quality) {
+        Item[] items = new Item[] { new Item(name, sellIn, quality) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+
+        return app.items[0].toString();
+    }
 }
