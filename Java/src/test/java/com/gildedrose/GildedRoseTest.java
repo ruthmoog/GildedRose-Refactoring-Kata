2561ca6b76530c49e7af67105cb0151c6a093d2b
Ruth Baker <ruthmoog@gmail.com>
2021-08-01 15:05:18 +0100

    Update test with mutation testing

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -11,7 +11,7 @@ class GildedRoseTest {
         CombinationApprovals.verifyAllCombinations(
             this::doUpdateQuality,
             new String[] {"Blue", "Aged Brie", "Backstage passes to a TAFKAL80ETC concert", "Sulfuras, Hand of Ragnaros"},
-            new Integer[] {-1, 0, 11},
+            new Integer[] {-1, 0, 5, 10, 11},
             new Integer[] {0, 1, 49, 50});
     }
 
