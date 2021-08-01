1e6fa66c65f35717381e71b4290b888387a44e8d
Ruth Baker <ruthmoog@gmail.com>
2021-08-01 14:48:36 +0100

    Increase code coverage to 100%

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -10,12 +10,12 @@ class GildedRoseTest {
     void updateQuality() {
         CombinationApprovals.verifyAllCombinations(
             this::doUpdateQuality,
-            new String[] {"Blue"},
-            new Integer[] {0},
-            new Integer[] {0});
+            new String[] {"Blue", "Aged Brie", "Backstage passes to a TAFKAL80ETC concert", "Sulfuras, Hand of Ragnaros"},
+            new Integer[] {-1, 0, 11},
+            new Integer[] {0, 1, 49, 50});
     }
 
-    private String doUpdateQuality(String name, int sellIn, int quality) {
+    private String doUpdateQuality(String name, Integer sellIn, Integer quality) {
         Item[] items = new Item[] { new Item(name, sellIn, quality) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
